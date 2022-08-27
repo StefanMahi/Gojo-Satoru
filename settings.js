d9ea2f239fcdec03507554281b85f5050554984c
Mahi <111268622+StefanMahi@users.noreply.github.com>
2022-08-27 08:27:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94707330571']
+global.premium = ['94707330571']
+global.ownernomer = '94707330571'
+global.ownername = '🔰Stefan Mahi🔰'
+global.botname = '🎮Mahi Botz🎮'
 global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.ig = 'https://github.com/nexusNwa'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = '🎮Mahi Botz🎮'
+global.author = '🔰Stefan Mahi🔰'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🐦','🐤','🎮']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
